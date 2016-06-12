98d919cac9d4bbc0838720007f622fb92f64474f
AllenZhang <zhangyang@shinetechchina.com>
2016-06-12 17:50:30 +0800

    init basic elements.

--- scripts/page/page.js
+++ scripts/page/page.js
@@ -1,23 +1,44 @@
-(function () {
+(function() {
     'use strict';
-    
-    angular.module('app').directive('weather', function () {
-    	return {
-    		restrict : 'E',
-    		transclude : false,
-    		templateUrl : 'scripts/page/page.html',
-    		scope : {
+    angular.module('app').directive('weather', function() {
+        return {
+            restrict: 'E',
+            transclude: false,
+            templateUrl: 'scripts/page/page.html',
+            scope: {},
+            required: ['$scope', '$http'],
+            controllerAs: 'vm',
+            bindToController: true,
+            controller: function($scope, $http) {
+                var vm = this;
+                var myToken = '30ba7757914543d690710f667c834dfa';
+                var cityList = {};
+                var baseUri = 'http://api.heweather.com/x3/';
+                vm.cityType = 'allchina';
+                vm.weatherTitle = 'zz';
+                vm.formData = {};
+                vm.getCityWeather = function() {
+                    getCityWeather(vm.selectedCityId, myToken)
+                        .success(function(data, status) {
+                            var key = "HeWeather data service 3.0";
+                            vm.formData = data;
+                    });
+                }
 
-    		},
-    		required : ['$scope', '$http'],
-    		controllerAs : 'vm',
-    		bindToController : true,
-    		controller : function ($scope, $http) {
-    			var vm = this;
-    			vm.weatherTitle = 'zz';
-    		}
-    	}
+                angular.element(document).ready(function() {
+                    getAllCities(vm.cityType, myToken).success(function(data, status) {
+                        vm.cityList = data.city_info;
+                    });
+                });
+
+                function getAllCities(cityType, token) {
+                    return $http.get(baseUri + 'citylist?search=' + cityType + '&key=' + token);
+                }
+
+                function getCityWeather(cityId, token) {
+                    return $http.get(baseUri + 'weather?cityid=' + cityId + '&key=' + token);
+                }
+            }
+        };
     });
-        
-
 })();
